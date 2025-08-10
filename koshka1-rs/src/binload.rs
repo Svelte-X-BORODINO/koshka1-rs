a2d0e81f4d0ffee7ab89147e3579078cc7262c87
zhdidoks123 <mshustov131213@gmail.com>
2025-08-10 16:46:16 +0300

    Commit 2

--- koshka1-rs/src/binload.rs
+++ koshka1-rs/src/binload.rs
@@ -5,3 +5,7 @@ use std::fs;
 fn binload(path: &str) -> Vec<u8> {
     fs::read(path).except("Failed to read binary")
 }
+
+fn prog_run(path: &str) {
+    binload(&path);
+}
--- koshka1-rs/src/cpu.rs
+++ koshka1-rs/src/cpu.rs
@@ -2,8 +2,24 @@ pub mod cpu;
 
 #[derive(Debug)]
 struct KoshkaCPU {
-    k[5]: u8,
-    kadv: String,
-    kadv2: String,
-    
+    k: [u8; 5],
+    kadv: &str,
+    kadv2: &str,
 }
+trait Funcs {
+    fn init_cpu(&mut self);
+    fn print_status(&self);
+    fn kadd(&mut self, a: i8, b: i8);
+    fn ksub(&mut self, a: i8, b: i8);
+    fn kmul(&mut self, a: i8, b: i8);
+    fn kdiv(&mut self, a: i8, b: i8);
+}
+
+impl Funcs for KoshkaCPU {
+    fn init_cpu(&mut self) {
+        self.k = [0; 5];
+        self.kadv = "".to_string();
+        self.kadv2 = "".to_string();
+        
+    }
+} 
