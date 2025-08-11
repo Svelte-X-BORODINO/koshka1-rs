f6948bdcaefab9a868cbfb8cbe82d87cc1794a1e
zhdidoks123 <mshustov131213@gmail.com>
2025-08-11 20:29:05 +0300

    Commit 4

---
+++ koshka1-rs/src/video.rs
@@ -0,0 +1,17 @@
+pub struct VideoController {
+    vram: [u8, 80 * 25];
+    buf: [u8, 80 * 25];
+} 
+
+trait Funcs {
+    fn init_video(&mut self);
+    fn disp(txt: &str);
+    fn dispd(msg: &str);
+}
+
+impl Funcs for VideoController {
+    fn init_cpu() {
+        
+        
+    }
+}
